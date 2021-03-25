f2eb6e1766f73d46c9a11703785d8b86e3d848d8
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-03-25 23:32:56 +0300

    fix: what-season fix

--- src/what-season.js
+++ src/what-season.js
@@ -4,21 +4,24 @@ module.exports = function getSeason(date) {
     if(!date) {
         return 'Unable to determine the time of year!'
     }
-    if(!date.getTimezoneOffset()) {
-        throw new Error('error')
+    if(date instanceof Date) {
+        if(Object.prototype.toString.call(date) !== '[object Date]') {
+            throw new Error("this fake date")
+        }
+      let numberMonth = date.getMonth();
+      if(numberMonth === 0 || numberMonth === 1 || numberMonth === 11) {
+          return "winter"
+      }
+      else if (numberMonth === 2 || numberMonth === 3 || numberMonth === 4) {
+          return "spring"
+      }
+      else if (numberMonth === 5 || numberMonth === 6 || numberMonth === 7) {
+          return "summer"
+      }
+      else if (numberMonth === 8 || numberMonth === 9 || numberMonth === 10) {
+          return "autumn"
+      }
     }
-  let numberMonth = date.getMonth();
-  if(numberMonth === 0 || numberMonth === 1 || numberMonth === 11) {
-      return "winter"
-  }
-  else if (numberMonth === 2 || numberMonth === 3 || numberMonth === 4) {
-      return "spring"
-  }
-  else if (numberMonth === 5 || numberMonth === 6 || numberMonth === 7) {
-      return "summer"
-  }
-  else if (numberMonth === 8 || numberMonth === 9 || numberMonth === 10) {
-      return "autumn"
-  }
-    
+
+    throw new Error('argument passed')
 };
