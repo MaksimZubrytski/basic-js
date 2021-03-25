37362f2452714214fb300535e87cfaaae80b0b63
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-03-25 23:17:33 +0300

    refactor: refactor what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,24 @@
-  const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if(!date) {
+        return 'Unable to determine the time of year!'
+    }
+    if(!date.getTimezoneOffset()) {
+        throw new Error('error')
+    }
+  let numberMonth = date.getMonth();
+  if(numberMonth === 0 || numberMonth === 1 || numberMonth === 11) {
+      return "winter"
+  }
+  else if (numberMonth === 2 || numberMonth === 3 || numberMonth === 4) {
+      return "spring"
+  }
+  else if (numberMonth === 5 || numberMonth === 6 || numberMonth === 7) {
+      return "summer"
+  }
+  else if (numberMonth === 8 || numberMonth === 9 || numberMonth === 10) {
+      return "autumn"
+  }
+    
 };
