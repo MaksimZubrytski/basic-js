89829ad661dffb98aceb05d10264d9ab7b6e5845
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-03-26 20:46:21 +0300

    feat: add solved vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,91 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isNotReverse = true) {
+    this._isNotReverse = isNotReverse;
+  }
+  encrypt(word, key) {
+    if (!word || !key) {
+      throw new Error('error')
+    }
+    let wordInUpperCase = word.toUpperCase();
+    let keyInUpperCase = key.toUpperCase().repeat(Math.ceil(word.length / key.length));
+
+    let arrayLetters = wordInUpperCase.split('');
+    let arrayLettersKey = keyInUpperCase.split('');
+    let newArray = [];
+
+    for (let i = 0; i < arrayLetters.length; i++) {
+      arrayLetters[i] = arrayLetters[i].codePointAt()
+      arrayLettersKey[i] = arrayLettersKey[i].codePointAt()
+    }
+
+    for (let i = 0, j = 0; i < arrayLetters.length; i++, j++) {
+      if (arrayLetters[i] > 90 || arrayLetters[i] < 65) {
+        newArray[i] = arrayLetters[i]
+        j--;
+        continue;
+      }
+      newArray[i] = arrayLetters[i] + arrayLettersKey[j];
+      if (newArray[i] > 155) {
+        newArray[i] = newArray[i] - 91;
+      } else {
+        newArray[i] = newArray[i] - 65;
+      }
+    }
+
+    for (let i = 0; i < newArray.length; i++) {
+      newArray[i] = String.fromCodePoint(newArray[i])
+    }
+    if (!this._isNotReverse) {
+      return newArray.reverse().join('')
+    }
+    return newArray.join('')
+  }
+
+  decrypt(word, key) {
+    if (!word || !key) {
+      throw new Error('error')
+    }
+
+    let wordInUpperCase = word.toUpperCase();
+    let keyInUpperCase = key.toUpperCase().repeat(Math.ceil(word.length / key.length));
+
+    let arrayLetters = wordInUpperCase.split('');
+    let arrayLettersKey = keyInUpperCase.split('');
+    let newArray = [];
+
+    for (let i = 0; i < arrayLetters.length; i++) {
+      arrayLetters[i] = arrayLetters[i].codePointAt()
+      arrayLettersKey[i] = arrayLettersKey[i].codePointAt()
+    }
+
+    for (let i = 0, j = 0; i < arrayLetters.length; i++, j++) {
+      if (arrayLetters[i] > 90 || arrayLetters[i] < 65) {
+        newArray[i] = arrayLetters[i]
+        j--;
+        continue;
+      }
+      newArray[i] = arrayLetters[i] - arrayLettersKey[j];
+      if (newArray[i] >= 0) {
+        newArray[i] = newArray[i] + 65;
+      } else {
+        newArray[i] = newArray[i] + 91;
+      }
+    }
+
+    for (let i = 0; i < newArray.length; i++) {
+      newArray[i] = String.fromCodePoint(newArray[i])
+    }
+
+    if (!this._isNotReverse) {
+      return newArray.reverse().join('')
+    }
+
+    return newArray.join('')
   }
 }
 
 module.exports = VigenereCipheringMachine;
+
+
